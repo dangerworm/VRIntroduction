843e7a1c39c73e210a9d9be6bc2e3205bf1ebfe9
Drew Morgan <dangerworm@gmail.com>
2019-04-07 11:32:29 +0100

    Added dynamic building placement, keyboard and mouse bindings, continuous rotation control and INERTIAL DAMPENERS!

---
+++ New Unity Project/Assets/InitialiseBuildings.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class InitialiseBuildings : MonoBehaviour
+{
+    public Transform BuildingTransform;
+    public Transform FloorTransform;
+
+    public int NumberOfBuildings;
+
+    // Use this for initialization
+    void Start()
+    {
+        var xRange = new RangeAttribute(-FloorTransform.localScale.x / 2, FloorTransform.localScale.x / 2);
+        var yRange = new RangeAttribute(3.0f, 100);
+        var zRange = new RangeAttribute(-FloorTransform.localScale.z / 2, FloorTransform.localScale.z / 2);
+
+        for (var i = 0; i < NumberOfBuildings; i++)
+        {
+            var xPosition = Random.Range(xRange.min * FloorTransform.localScale.x, xRange.max * FloorTransform.localScale.x);
+            var zPosition = Random.Range(zRange.min * FloorTransform.localScale.z, zRange.max * FloorTransform.localScale.z);
+
+            var xScale = Random.Range(xRange.min, xRange.max);
+            var yScale = Random.Range(yRange.min, yRange.max);
+            var zScale = Random.Range(zRange.min, zRange.max);
+
+            var position = new Vector3(xPosition, yScale / 2, zPosition);
+            var scale = new Vector3(xScale, yScale, zScale);
+
+            var newBuilding = Instantiate(BuildingTransform, position, Quaternion.identity);
+            newBuilding.localScale = scale;
+        }
+    }
+}
--- New Unity Project/Assets/ShipCode.cs
+++ New Unity Project/Assets/ShipCode.cs
@@ -1,94 +1,149 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.UIElements;
 
 public class ShipCode : MonoBehaviour
 {
     public Rigidbody ShipBody;
     public Transform ShipTransform;
+    public float EnginePower;
+    public float VelocityDampeningFactor;
+    public float RotationalDampeningFactor;
+    public bool UseContinuousRotation;
+    public bool ThrustControlActive;
+    public bool StabilisersActive;
 
-    private Dictionary<KeyCode, Vector3> _keyRotations;
+    private Dictionary<KeyCode, Vector3> _rotationKeys;
     private Vector3 _rotation;
 
     private float _dt;
-    private float _dSpeed;
-    private float _dBrake;
     private float _dRotation;
 
+    private bool _brakeActive;
     private bool _cursorLocked;
-    private bool _useContinuousRotation;
 
     // Use this for initialization
     void Start()
     {
+        _brakeActive = false;
         _cursorLocked = true;
-        _useContinuousRotation = false;
+        _dRotation = 20.0f;
 
-        _dSpeed = 60.0f;
-        _dBrake = 1 + (1 / _dSpeed);
+        _rotation = UseContinuousRotation 
+            ? new Vector3(0.0f, 0.0f, 0.0f) 
+            : ShipTransform.localEulerAngles;
 
-        if (_useContinuousRotation)
+        _rotationKeys = new Dictionary<KeyCode, Vector3>
         {
-            _rotation = new Vector3(0, 0, 0);
-            _dRotation = 0.5f;
-        }
-        else
-        {
-            _rotation = ShipTransform.localEulerAngles;
-            _dRotation = 30.0f;
-        }
-
-        _keyRotations = new Dictionary<KeyCode, Vector3>
-        {
-            { KeyCode.A, new Vector3 (0, -1, 0) },
-            { KeyCode.D, new Vector3 (0, 1, 0) }
+            { KeyCode.W, new Vector3( 1,  0,  0) },
+            { KeyCode.S, new Vector3(-1,  0,  0) },
+            { KeyCode.A, new Vector3( 0, -1,  0) },
+            { KeyCode.D, new Vector3( 0,  1,  0) },
+            { KeyCode.Q, new Vector3( 0,  0,  1) },
+            { KeyCode.E, new Vector3( 0,  0, -1) }
         };
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.BackQuote))
+        ProcessInputs();
+        ApplyRotation();
+
+        if (ThrustControlActive)
         {
-            _cursorLocked = !_cursorLocked;
+            ApplyInertialDampeners();
         }
 
-        if (Input.GetKey(KeyCode.W))
+        Cursor.lockState = _cursorLocked ? CursorLockMode.Locked : CursorLockMode.None;
+    }
+
+    private void ProcessInputs()
+    {
+        ProcessMouse();
+        ProcessKeyboard();
+    }
+
+    private void ProcessMouse()
+    {
+        if (Input.GetMouseButton((int)MouseButton.LeftMouse))
         {
-            ShipBody.AddForce(ShipTransform.forward * Time.deltaTime * _dSpeed);
+            ShipBody.AddForce(ShipTransform.forward * Time.deltaTime * EnginePower);
         }
 
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetMouseButton((int)MouseButton.RightMouse))
         {
-            ShipBody.AddForce(-ShipTransform.forward * Time.deltaTime * _dSpeed);
+            ShipBody.AddForce(-ShipTransform.forward * Time.deltaTime * EnginePower);
+        }
+    }
+
+    private void ProcessKeyboard()
+    {
+        foreach (var key in _rotationKeys.Keys)
+        {
+            if (Input.GetKey(key))
+            {
+                _rotation += _rotationKeys[key] * Time.deltaTime * _dRotation * (StabilisersActive ? RotationalDampeningFactor : 1);
+            }
         }
 
         if (Input.GetKey(KeyCode.Space))
         {
-            ShipBody.velocity = ShipBody.velocity / _dBrake;
+            _brakeActive = true;
         }
 
-        foreach (var key in _keyRotations.Keys)
+        if (Input.GetKeyDown(KeyCode.T))
         {
-            if (Input.GetKey(key))
-            {
-                _rotation += _keyRotations[key] * Time.deltaTime * _dRotation;
-            }
+            ThrustControlActive = !ThrustControlActive;
         }
 
-        if (_useContinuousRotation)
+        if (Input.GetKeyDown(KeyCode.R))
         {
-            ShipTransform.localEulerAngles += _rotation;
-        }
-        else
-        {
-            ShipTransform.localEulerAngles = _rotation;
+            StabilisersActive = !StabilisersActive;
         }
 
-        // Slow down sideways movement
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            UseContinuousRotation = !UseContinuousRotation;
+        }
+    }
+
+    private void ApplyRotation()
+    {
+        if (!(Math.Abs(_rotation.x + _rotation.y + _rotation.z) > Time.deltaTime * 0.1))
+            return;
+
+        ShipTransform.Rotate(_rotation);
+
+        if (!UseContinuousRotation)
+        {
+            _rotation = new Vector3(0.0f, 0.0f, 0.0f);
+        }
+        else if (StabilisersActive)
+        {
+            ApplyStabilisers();
+        }
+    }
+
+    private void ApplyInertialDampeners()
+    {
         var localVelocity = ShipTransform.InverseTransformVector(ShipBody.velocity);
-        localVelocity.x /= _dBrake;
-        ShipBody.velocity = ShipTransform.TransformDirection(localVelocity);
 
-        Cursor.lockState = _cursorLocked ? CursorLockMode.Locked : CursorLockMode.None;
+        localVelocity.x /= VelocityDampeningFactor;
+        localVelocity.y /= VelocityDampeningFactor;
+
+        if (_brakeActive)
+        {
+            localVelocity.z /= VelocityDampeningFactor;
+            _brakeActive = false;
+        }
+
+        ShipBody.velocity = ShipTransform.TransformDirection(localVelocity);
+    }
+
+    private void ApplyStabilisers()
+    {
+        _rotation /= RotationalDampeningFactor;
     }
 }
